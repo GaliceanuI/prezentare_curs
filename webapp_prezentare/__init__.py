a0b4d9f3f8a560727336cb74590b12d3a6141ed4
Microsoft Learn Student <learn@contoso.com>
2023-07-13 13:53:00 +0300

    added new settings to api

--- webapp_prezentare/__init__.py
+++ webapp_prezentare/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = "IT School"
+    name = req.params.get('name')
     if not name:
         try:
             req_body = req.get_json()
